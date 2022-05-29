213bf66f2301f451ffc9c5e5100a9e7648296e8a
pradeep <pradeep.misra@gmail.com>
2022-05-29 12:26:18 +0530

    Implemented scss for styling

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
+import './Styles.css';
 
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
+      <h2>Login</h2>
       <Navigation onLogout={props.onLogout} />
     </header>
   );
